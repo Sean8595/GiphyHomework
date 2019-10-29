dedce62f42a956a24b9ce3cd235ff5fc8ad37f54
Sean Melvin <seanmelvin8595@gmail.com>
2019-10-29 18:49:43 -0400

    buggy but sooo close

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,11 @@
+$(document).ready(function(){
+
 var topicArray = ["Dipper","Mable","Sues","Gruncle Stan" ]
-$(document).ready(function renderButtons() {
+function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < topicArray.length; i++) {
       var a = $("<button>");
-      a.addClass("movie");
+      a.addClass("Movie");
       a.attr("data-name", topicArray[i]);
       a.text(topicArray[i]);
       $("#buttons-view").append(a);
@@ -11,39 +13,53 @@ $(document).ready(function renderButtons() {
     }
     $("#add-movie").on("click", function(event) { //should push movie input into the topic 
       event.preventDefault();
-      var topic = $("#movie-input").val()
+      var topic = $("#movie-input").val();
       topicArray.push(topic);
-      renderButtons();
+      renderButtons()
     })
   }
-)
-
-
+renderButtons()
 
 // This line grabs the input from the textbox
-$(".movie")
+$(".Movie").on("click",function(){
 //setting variables and getting the API in place
   var gifs = $(this).attr("data-name");
   var queryURL =
+
     "https://api.giphy.com/v1/gifs/search?api_key=jv42QTYRvm7KTJ5OHbztKH9DAnfoau9X&q=" +
-    gifs +
-    "&limit=25&offset=0&rating=G&lang=en";
-  // Creating an AJAX call
-  
+    gifs + "&limit=25&offset=0&rating=G&lang=en";
+
+  console.log("clicked")
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(data) {
-//creates div to put into
+
     var gifDiv = $("<div class='gif'>");
-    console.log(data);
+
 //pulls data from giphy
-    var rating = data.data[0].rating;
-//creates divs to put the information in
-    var pOne = $("<p>").text("Rating: " + rating);
-//apends the previous information
-    gifDiv.append(pOne);
-  });
+for (i = 0; i < 10; i++) {
+    var image = $("<img>")
+    var stillImgURL = data.data[i].images.fixed_height_small_still.url;
+    var animateImgURL = data.data[i].images.fixed_height_small.url;
+    var rating = data.data[i].rating
+
+    image.attr("src", stillImgURL).addClass("gif");
+    image.attr("data-state", "still");
+    image.attr("data-animate", animateImgURL);
+    image.attr("data-still", stillImgURL);
+
+    console.log(animateImgURL);
+
+    gifDiv.append(image);
+    gifDiv.append(rating);
+ $("#movies-view").prepend(gifDiv)
 }
 
- 
+    console.log(rating)
+//apends the previous information
+  });
+
+})
+})
