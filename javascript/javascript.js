0709130b6d458f59bc33acfa26ed8e26e3077623
Sean Melvin <seanmelvin8595@gmail.com>
2019-10-29 09:32:42 -0400

    words

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,15 @@
 function displayGif() {
-
+//setting variables and getting the API in place
     var giphy = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/" + movie + "&apikey=goNPnFIy2mmPfq7UKC4MprL6EOCj5F5H";
-
-    // Creating an AJAX call for the specific movie button being clicked
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=jv42QTYRvm7KTJ5OHbztKH9DAnfoau9X&q=banana&limit=25&offset=0&rating=G&lang=en";
+    var gifs = []
+// Creating an AJAX call
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response)
+    }).then(function(data){
+    // var gifDiv = $("<div class='gif'>");
+    console.log(data)
+    })}
+
+    displayGif();
