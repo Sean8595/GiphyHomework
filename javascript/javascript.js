f557842c580c7bd5cece69ba87da22934eeecd63
Sean Melvin <seanmelvin8595@gmail.com>
2019-10-29 20:12:04 -0400

    fixed double button bug

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
-
-var topicArray = ["Dipper Pines","Mable Pines","Soos","Gruncle Stan" ]
-function renderButtons() {
+$(document).ready(function() {
+  var topicArray = ["Dipper Pines", "Mable Pines", "Soos", "Gruncle Stan","Bill Cypher","Tyler Cutebiker"];
+  function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < topicArray.length; i++) {
       var a = $("<button>");
@@ -9,68 +8,67 @@ function renderButtons() {
       a.attr("data-name", topicArray[i]);
       a.text(topicArray[i]);
       $("#buttons-view").append(a);
-    
     }
-    $("#add-movie").on("click", function(event) { //should push movie input into the topic 
-      event.preventDefault();
-      var topic = $("#movie-input").val();
-      topicArray.push(topic);
-      renderButtons()
-    })
   }
-renderButtons()
-
-// This line grabs the input from the textbox
-$(".Movie").on("click",function(){
-//setting variables and getting the API in place
-  var gifs = $(this).attr("data-name");
-  var queryURL =
-
-    "https://api.giphy.com/v1/gifs/search?api_key=jv42QTYRvm7KTJ5OHbztKH9DAnfoau9X&q=" +
-    gifs + "&limit=25&offset=0&rating=G&lang=en";
-
-  console.log("clicked")
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(data) {
-
-    var gifDiv = $("<div class='gif'>");
-
-//pulls data from giphy
-for (i = 0; i < 10; i++) {
-    var image = $("<img>")
-    //ajay helped me a lot with this section!
-    var stillImgURL = data.data[i].images.fixed_height_small_still.url;
-    var animateImgURL = data.data[i].images.fixed_height_small.url;
-    // var rating = data.data[i].rating
-
-    image.attr("src", stillImgURL).addClass("gif");
-    image.attr("data-state", "still");
-    image.attr("data-animate", animateImgURL);
-    image.attr("data-still", stillImgURL);
-    image.addClass("borders");
-    gifDiv.append(image);
-    // gifDiv.append(rating);
- $("#movies-view").prepend(gifDiv)
-}
-
-//gets it animated, took this from the activity 
-$(document).on("click", ".gif", function () {
-var state = $(this).attr("data-state");
-var animateSrc = $(this).attr("data-animate");
-var stillSrc = $(this).attr("data-still");
-if (state === "still") {
-    $(this).attr("src", animateSrc);
-    $(this).attr("data-state", "animate");
-} else if (state === "animate") {
-    $(this).attr("src", stillSrc);
-    $(this).attr("data-state", "still");
-}
-    console.log(rating)
-//apends the previous information
+  renderButtons();
+  $("#add-movie").on("click", function(event) {
+    //should push movie input into the topic
+    event.preventDefault();
+    var topic = $("#movie-input").val();
+    topicArray.push(topic);
+    console.log(topicArray)
+    renderButtons();
   });
+  // This line grabs the input from the textbox
+  $(".Movie").on("click", function() {
+    //setting variables and getting the API in place
+    var gifs = $(this).attr("data-name");
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?api_key=jv42QTYRvm7KTJ5OHbztKH9DAnfoau9X&q=" +
+      gifs +
+      "&limit=25&offset=0&rating=G&lang=en";
 
-})
-})})
+    console.log("clicked");
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(data) {
+      var gifDiv = $("<div class='gif'>");
+
+      //pulls data from giphy
+      for (i = 0; i < 10; i++) {
+        var image = $("<img>");
+        //ajay helped me a lot with this section!
+        var stillImgURL = data.data[i].images.fixed_height_small_still.url;
+        var animateImgURL = data.data[i].images.fixed_height_small.url;
+        // var rating = data.data[i].rating
+
+        image.attr("src", stillImgURL).addClass("gif");
+        image.attr("data-state", "still");
+        image.attr("data-animate", animateImgURL);
+        image.attr("data-still", stillImgURL);
+        image.addClass("borders");
+        gifDiv.append(image);
+        // gifDiv.append(rating);
+        $("#movies-view").prepend(gifDiv);
+      }
+
+      //gets it animated, took this from the activity
+      $(document).on("click", ".gif", function() {
+        var state = $(this).attr("data-state");
+        var animateSrc = $(this).attr("data-animate");
+        var stillSrc = $(this).attr("data-still");
+        if (state === "still") {
+          $(this).attr("src", animateSrc);
+          $(this).attr("data-state", "animate");
+        } else if (state === "animate") {
+          $(this).attr("src", stillSrc);
+          $(this).attr("data-state", "still");
+        }
+        console.log(rating);
+        //apends the previous information
+      });
+    });
+  });
+});
